133f03cf98e85335d354a42f05132cb6d4cc91fd
matheusbene1207-code <matheusbene1207@gmail.com>
2026-09-23 12:03:49 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,25 @@
+// let nome = 'Matheus'
+// let idade = 15
+// let altura = 1.88
+
+// console.log(`Meu nome é ${nome}, tenho ${idade} anos de idade e ${altura} minha altura`)
+
+
+// let numero1 = 18
+// let numero2 = 18
+
+
+// let soma = numero1 + numero2
+// let subtraçao = numero1 - numero2
+// let multiplicaçao = numero1 * numero2
+// let divisao = numero1 / numero2
+
+// console.log(soma)
+// console.log(subtraçao)
+// console.log(multiplicaçao)
+// console.log(divisao)
+
+
+
+
+
