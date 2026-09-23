90354e8f574db29fd247ed1f3a20cb1aefc77c59
matheusbene1207-code <matheusbene1207@gmail.com>
2026-09-23 12:03:20 -0300

    Add files via upload

---
+++ excer.js
@@ -0,0 +1,13 @@
+let array = [1, 2, 3, 4, 5]
+
+console.log(array)
+array.push(6)
+console.log(array)
+array.unshift(0)
+console.log(array)
+array.pop(3)
+console.log(array)
+
+let cores = ['preto', 'vermelho', 'branco', 'azul', 'cinza' ]
+cores [2] = 'rosa'
+console.log(cores)
